3a0b9abdf150d738018ef5605a6a52ed8c1a9172
Andy Shih <AndyShih51@gmail.com>
2019-10-26 00:43:41 -0500

    Add image_recognition module

---
+++ modules/image_recognition.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import boto3
+import sys
+
+AWS_REGION='us-west-2'
+AWS_ACCESS_KEY_ID='AKIAJIOPZPVLIEAWBCPA'
+AWS_SECRET_ACCESS_KEY='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'
+
+REKOGNITION_CLIENT = boto3.client(
+    'rekognition',
+    region_name=AWS_REGION,
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+)
+
+def find_squirrel_confidence(image_file):
+    # Convert image to bytearray
+    with open(image_file, 'rb') as image:
+        bytes = bytearray(image.read())
+    # Make API call to get labels
+    response = REKOGNITION_CLIENT.detect_labels(
+        Image={ 'Bytes': bytes },
+        MinConfidence=20,
+    )
+    # Process each label
+    for label in response['Labels']:
+        print('({:.2f}%) {}'.format(label['Confidence'], label['Name']))
+        if label['Name'] == 'Squirrel':
+            return label['Confidence']
+    return 0
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('usage: main.py image_file ...')
+        exit()
+    
+    image_files = sys.argv[1:]
+
+    for image_file in image_files:
+        confidence = find_squirrel_confidence(image_file)
+        print(image_file)
+        if confidence > 0:
+            print('It\'s a squirrel ({:.2f}%)!'.format(confidence))
+        else:
+            print('Nope')
+        print()
