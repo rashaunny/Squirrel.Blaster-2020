57d60d8060a8601c7dff61fffb48079ce2a9ebf8
Andy Shih <AndyShih51@gmail.com>
2019-10-26 15:51:18 -0500

    Add untested camera script

---
+++ camera_loop.py
@@ -0,0 +1,31 @@
+from picamera import PiCamera
+import time
+
+from modules.image_recognition import find_squirrel_confidence
+
+def init_camera():
+    camera = PiCamera()
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    time.sleep(2)
+    return camera
+
+def look_for_squirrel(camera, i):
+    filename = 'tmp-{}.jpg'.format(i)
+    camera.capture(filename)
+    confidence = find_squirrel_confidence(filename)
+
+    print(i)
+    if confidence > 0:
+        print('It\'s a squirrel ({:.2f}%)!'.format(confidence))
+    else:
+        print('Nope')
+    print()
+
+
+if __name__ == "__main__":
+    camera = init_camera()
+
+    for i in range(10):
+        look_for_squirrel(camera, i)
+        sleep(2)
