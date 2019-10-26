38c686b7c483722395ba86eb16c9d684f94c4e00
Andy Shih <AndyShih51@gmail.com>
2019-10-26 17:43:54 -0500

    Refactor

--- modules/camera.py
+++ modules/camera.py
@@ -1,24 +1,29 @@
 from picamera import PiCamera
 import time
+import os
+import shutil
 
 from modules.image_recognition import find_squirrel_confidence
-
-def init_camera():
-    camera = PiCamera()
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    time.sleep(2)
-    return camera
-
-CAMERA = init_camera()
-picture_id = 0
 
 def get_squirrel_confidence():
     global picture_id
 
-    filename = 'tmp-{}.jpg'.format(picture_id)
+    filename = '{}/tmp-{}.jpg'.format(PICTURE_DIR, picture_id)
     picture_id += 1
     
     CAMERA.capture(filename)
     confidence = find_squirrel_confidence(filename)
     return confidence
+
+PICTURE_DIR = 'CameraPictures'
+picture_id = 0
+
+# Initialize camera
+CAMERA = PiCamera()
+CAMERA.resolution = (1024, 768)
+CAMERA.start_preview()
+time.sleep(2)
+
+# Delete old camera pictures
+shutil.rmtree(PICTURE_DIR)
+os.mkdir(PICTURE_DIR, exist_ok=True)
